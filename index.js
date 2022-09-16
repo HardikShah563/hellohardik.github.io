3a487fa29a5dbe671994d66e6119ed47c7107c8a
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2022-09-16 23:18:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
-const navSlide = () => {
-    const lines = document.querySelector('.lines');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    //Toggle Nav
-    lines.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-
-        // Animate Links
-        navLinks.forEach((link, index) => {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
-            console.log(index / 5 + 0.5);
-        });
-
-        // Lines Animation
-        lines.classList.toggle('toggle');
-    });
-}
-navSlide();
-
+// Cursor div drags behind the cursor
 const cursor = document.querySelector('.cursor');
 document.addEventListener("mousemove", (e) => {
     let leftPosition = e.pageX;
     let topPosition = e.pageY;
-
     cursor.style.left = leftPosition + "px";
     cursor.style.top = topPosition + "px";
+});
+
+// Navbar CSS
+var navLines = document.querySelector(".nav-lines");
+navLines.addEventListener("click", () => {
+    var navLinks = document.querySelector(".nav-links");
+    navLinks.classList.toggle("active");
 })
-const navLinkItems = document.querySelectorAll('nav ul li');
-navLinkItems.forEach(link => {
-    link.addEventListener("mouseenter", () => {
-        cursor.classList.add("large");
-    })
-})
-navLinkItems.forEach(link => {
-    link.addEventListener("mouseleave", () => {
-        cursor.classList.remove("large");
-    })
-})
+
+
+
+// const navLinkItems = document.querySelectorAll('nav ul li');
+// navLinkItems.forEach(link => {
+//     link.addEventListener("mouseenter", () => {
+//         cursor.classList.add("large");
+//     })
+// })
+// navLinkItems.forEach(link => {
+//     link.addEventListener("mouseleave", () => {
+//         cursor.classList.remove("large");
+//     })
+// })
