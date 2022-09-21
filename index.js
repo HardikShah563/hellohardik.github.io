0cdfd588638d2f49083f108e5936c080f1623bb6
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2022-09-22 00:50:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ navLines.addEventListener("click", () => {
 
 // My Stack printting divs: 
 var stackName = ['HTML', 'CSS', 'JavaScript', 'React', 'Node JS', 'Python', 'C', 'C++', 'Github', 'npm', 'figma', 'Photoshop', 'PremierePro'];
-var stackPercentage = ['90', '70', '40', '40', '20', '0', '40', '20', '60', '70', '10', '30', '60', '80'];
+var stackPercentage = ['90', '70', '50', '40', '20', '60', '40', '30', '60', '70', '10', '30', '80'];
 
-var subtitle = document.getElementById("subtitle");
+var subtitle = document.getElementById("stack-title");
 
 function createStack() {
     for (let i = stackName.length - 1; i >= 0; i--) {
@@ -37,7 +37,7 @@ function createStack() {
             <p>${stackName[i]}</p>
         </div>
         <div class="rank-bar">
-            <div class="ranking"></div>
+            <div class="ranking" style="width: ${stackPercentage[i]}%"></div>
         </div>
         <div class="stack-percentage">
             <p>${stackPercentage[i]}%</p>
@@ -48,11 +48,6 @@ function createStack() {
     console.log("CreateStack works");
 }
 createStack();
-
-// stack:hover -> edit css of different classes
-var rankBar = document.querySelector(".rank-bar");
-var ranking = document.querySelector(".ranking");
-
 
 // const navLinkItems = document.querySelectorAll('nav ul li');
 // navLinkItems.forEach(link => {
