f4c45f9408d8456ff7f09c0b042d28a0bd232c5e
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2022-09-24 20:41:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Varaible declaration: 
+let i = 0;
+
 // Cursor div drags behind the cursor
 const cursor = document.querySelector('.cursor');
 document.addEventListener("mousemove", (e) => {
@@ -30,8 +33,8 @@ var stackPercentage = ['90', '70', '50', '40', '20', '60', '40', '30', '60', '70
 var subtitle = document.getElementById("stack-title");
 
 function createStack() {
-    for (let i = stackName.length - 1; i >= 0; i--) {
-        console.log(stackName[i]);
+    for (i = stackName.length - 1; i >= 0; i--) {
+        // console.log(stackName[i]);
         let newStack = `<div class="td">
         <div class="stack-name">
             <p>${stackName[i]}</p>
@@ -48,6 +51,65 @@ function createStack() {
     console.log("CreateStack works");
 }
 createStack();
+
+function createDivider() {
+    for (i = 0; i < 50; i ++) {
+        let slashSectionDivider = document.getElementById("divider-img");
+        let newSlashDivider = `
+        <img id="divider-img" src="./res/divider-paralellogram.svg" alt="">
+        `;
+        slashSectionDivider.insertAdjacentHTML("afterend", newSlashDivider);
+    }
+}
+createDivider();
+
+// Function to convert String to Binary String
+function strToBinary(str) {
+    let len = str.length;
+    let output = '';
+    for (let i = 0; i < len; i++) {
+        // convert each char to
+        // ASCII value
+        let val = (str[i]).charCodeAt(0);
+
+        // Convert ASCII value to binary
+        let bin = "";
+        while (val > 0) {
+            if (val % 2 == 1) {
+                bin += '1';
+            }
+            else
+                bin += '0';
+            val = Math.floor(val / 2);
+        }
+        bin = reverse(bin);
+
+        output = output.concat(bin);
+        output = output + ' ';
+    }
+    console.log(output);
+    return output;
+}
+
+function reverse(input) {
+    a = input.split("");
+    let l, r = 0;
+    r = a.length - 1;
+
+    for (l = 0; l < r; l++, r--) {
+        // Swap values of l and r
+        let temp = a[l];
+        a[l] = a[r];
+        a[r] = temp;
+    }
+    return (a).join("");
+}
+
+let stringToBePassed = 'STR';
+strToBinary(stringToBePassed);
+
+let binary = document.getElementById("exp-binary");
+binary.textContent = '>> ' + strToBinary('EXP');
 
 // const navLinkItems = document.querySelectorAll('nav ul li');
 // navLinkItems.forEach(link => {
