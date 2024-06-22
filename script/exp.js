39f286d26f1003f0929f1480165ca09edb82accd
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2024-06-22 16:02:02 +0530

    adding experience

--- script/exp.js
+++ script/exp.js
@@ -1,22 +1,27 @@
 var exp = [
     {
+        title: 'Tata Consultancy Services',
+        position: 'Project Intern',
+        duration: '[Sept 22 - Dec 22]'
+    },
+    {
+        title: 'Software Developer',
+        position: 'Dream Skrin',
+        duration: '[Dec 21 - Jan 22]'
+    },
+    {
         title: 'Helbent Labs',
         position: 'Web Developer',
         duration: '[Sept 22 - Dec 22]'
-    }, 
+    },
     {
         title: 'KJSCE CodeCell',
         position: 'Marketing Team',
         duration: '[Sept 22 - --- 23]'
-    }, 
+    },
     {
         title: 'Team KJSCE Robocon',
         position: 'Management Team',
         duration: '[Oct 21 - Aug 22]'
-    }, 
-    {
-        title: 'E-Cell IIT Hyderabad',
-        position: 'Campus Ambasaddor',
-        duration: '[Dec 21 - Jan 22]'
     }
 ];
